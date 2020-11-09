6d2a683303489a07eced97cb0a5937ecd22fb85b
Doge <guiqiqi187@gmail.com>
2020-11-09 15:09:46 +0800

    Sort Macros

--- header.h
+++ header.h
@@ -1,6 +1,15 @@
 #ifndef GRAYSET_HEADER_H
 #define GRAYSET_HEADER_H
 
+#define DEBUG
+#undef DEBUG
+#define MAX_GRAY_POWER_INPUT 10
+#define MIN_GRAY_POWER_INPUT 0
+
+// Control for multiset support
+#define MULTISET
+//#undef MULTISET
+
 #include <iostream>
 #include <memory>
 #include <string>
